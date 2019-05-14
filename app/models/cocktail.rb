a4bcb3d5345d8123a6d6b18212ba064b8adb4b9b
Ana Ghidelli <ana.ghidelli@gmail.com>
2019-05-14 12:31:22 +0100

    red dots

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,5 @@
 class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredient, through: :doses
+  validates :name, uniqueness: true, presence: true
 end
--- app/models/ingridient.rb
+++
@@ -1,2 +0,0 @@
-class Ingridient < ApplicationRecord
-end
