fe42ca248674e7ab9dae82279bd3ce2879c31720
Ana Ghidelli <ana.ghidelli@gmail.com>
2019-05-14 13:07:10 +0100

    fixing up routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails do # , only: [:create, :index, :destroy]
+    resources :doses, only: [:new, :create]
+  end
+  delete '/doses/:id', to: 'doses#destroy'
 end
